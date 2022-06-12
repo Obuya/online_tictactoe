c35493db70e93279e3f87d5c92520f4cae33ffbd
obu <ayubosman12@hotmail.com>
2022-06-12 11:20:07 -0400

    moved server files to root

--- client/src/components/square.js
+++
@@ -1,9 +0,0 @@
-const Square = (props) => {
-    return(
-        <div className = "square" id ={props.id}  grid-item = {props.gridpos} onClick = {props.onClick}>
-            {props.value}
-        </div>
-    )
-}
-
-export default Square
