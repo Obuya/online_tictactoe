e17bd5ed1a99ce17bc338ac40c3ce35aed111670
obu <ayubosman12@hotmail.com>
2022-06-12 22:48:33 -0400

    changed link to heroku server

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import {
 
 //server
 // "https://obuyatictactoe.herokuapp.com"
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://obuyatictactoe.herokuapp.com")
 
 
 
